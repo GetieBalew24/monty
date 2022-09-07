18cc789560d66b48656c1d78398e1ec8786481e3
GetieBalew24 <getiebalew2020@gmail.com>
2022-09-07 06:18:20 -0700

    header

--- monty.h
+++ monty.h
@@ -58,4 +58,5 @@ typedef struct instruction_s
 void m_push(stack_t **stack, unsigned int line_number);
 void m_pall(stack_t **stack, unsigned int line_number);
 stack_t *add_node(stack_t **stack, const int n);
+void op_pint(stack_t **stack, unsigned int line_number);
 #endif
