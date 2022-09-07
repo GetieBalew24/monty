3cdfbc5a2d299d02c1eb936e9b9153f5c96b8c6a
GetieBalew24 <getiebalew2020@gmail.com>
2022-09-07 06:20:02 -0700

    print

--- print.c
+++ print.c
@@ -1,19 +1,17 @@
 #include "monty.h"
 
 /**
- *  m_pint - print value on top of `stack', or exit if stack is empty
- *  @stack: double pointer to head of stack
- *  @line_number: line number of current operation
- *  Return: void
+ * op_pint - prints the value at the top of the stack,
+ * followed by a new line.
+ * @stack: the memory
+ * @line_number: the line
  */
-void m_pint(stack_t **stack, unsigned int line_number)
+void op_pint(stack_t **stack, unsigned int line_number)
 {
-	stack_t *head = *stack;
-
-	if (var.stack_len == 0)
+	if (!stack || !*stack)
 	{
-		dprintf(STDOUT_FILENO, "L%u: can't pint, stack empty\n",	line_number);
+		printf("L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", head->n);
+	printf("%d\n", (*stack)->n);
 }
