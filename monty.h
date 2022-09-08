85b41323e6bee6f283a245629cb995c52bc1fbe7
GetieBalew24 <getiebalew2020@gmail.com>
2022-09-08 13:42:28 -0700

    Header file

--- monty.h
+++ monty.h
@@ -1,38 +1,22 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-
-/**
- * struct var_s - struct to contain the main variables of the Monty interpreter
- * @queue: flag to determine if in stack vs queue mode
- * @stack_len: length of the stack
- */
-typedef struct var_s
-{
-	int queue;
-	size_t stack_len;
-} var_t;
-
-#define STACK 0
-#define QUEUE 1
-
-/* global struct to hold flag for queue and stack length */
-extern var_t var;
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
+ *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
@@ -47,7 +31,7 @@ typedef struct stack_s
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -55,10 +39,40 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void m_push(stack_t **stack, unsigned int line_number);
-void m_pall(stack_t **stack, unsigned int line_number);
-stack_t *add_node(stack_t **stack, const int n);
+/**
+ * struct GlobalStruct - contains all globals
+ * @arg: arguments
+ * @input: the input
+ * @stack_queue: the stack or queue
+ * @file: the file
+ */
+typedef struct Global_struct
+{
+	char *arguments;
+	char *input;
+	int stack_queue;
+	FILE *file;
+} global_variables;
+
+extern global_variables argument_container;
+global_variables argument_container;
+
+int isnum(char *str);
+void free_stack(stack_t **head);
+void get_opcode(stack_t **stack, unsigned int line_number, char *command);
+void create_buff(char *f_name);
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_sub(stack_t **stack, unsigned int line_number);
+void op_div(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
+void op_mod(stack_t **stack, unsigned int line_number);
+void nasus(stack_t **stack, stack_t *fresh);
+void veigar(stack_t **stack, stack_t *fresh);
 
-#endif
+#endif /* Life of Brian */
