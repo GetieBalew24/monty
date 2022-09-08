5111112f50b07603ce65913c172345df86fb1b1e
GetieBalew24 <getiebalew2020@gmail.com>
2022-09-08 13:44:36 -0700

    pushan element to the stack

--- push.c
+++ push.c
@@ -1,48 +1,30 @@
 #include "monty.h"
-#include <ctype.h>
-#include <stdio.h>
-
 /**
- * check_for_digit - checks that a string only contains digits
- * @arg: string to check
- * Return: 0 if only digits, else 1
+ * op_push - pushes integers to the stack or queue still working on edge cases
+ * @stack: the memory
+ * @line_number: the line
  */
-static int check_for_digit(char *arg)
+void op_push(stack_t **stack, unsigned int line_number)
 {
-	int i;
+	stack_t *fresh;
 
-	for (i = 0; arg[i]; i++)
+	if (!stack)
+		exit(EXIT_FAILURE);
+	if (!(isnum(argument_container.arguments)))
 	{
-		if (arg[i] == '-' && i == 0)
-			continue;
-		if (isdigit(arg[i]) == 0)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * m_push - push an integer onto the stack
- * @stack: double pointer to the beginning of the stack
- * @line_number: script line number
- * Return: void
- */
-void m_push(stack_t **stack, unsigned int line_number)
-{
-	char *arg;
-	int n;
-
-	arg = strtok(NULL, "\n\t\r ");
-	if (arg == NULL || check_for_digit(arg))
-	{
-		printf("%d \n", line_number);
+		printf("L%u: usage: push integer\n", line_number);
+		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	n = atoi(arg);
-	if (!add_node(stack, n))
+	fresh = malloc(sizeof(stack_t));
+	if (fresh == NULL)
 	{
 		printf("Error: malloc failed\n");
+		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
-	var.stack_len++;
+	if (argument_container.stack_queue == 0)
+		nasus(stack, fresh);
+	else
+		veigar(stack, fresh);
 }
