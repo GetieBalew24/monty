5cf8b5c89958b4326f73dd7c85567e7b508ae33c
GetieBalew24 <getiebalew2020@gmail.com>
2022-09-08 13:45:30 -0700

    print an element from stack

--- pall.c
+++ pall.c
@@ -1,24 +1,20 @@
 #include "monty.h"
-
 /**
- * m_pall - print all values on `stack' starting from the top
- * @stack: double pointer to head of stack
- * @line_number: line number being executed from script file
- * Return: void
- */
-void m_pall(stack_t **stack, unsigned int line_number)
+* op_pall - prints out the stack
+* @stack: the stack
+* @line_number: the line
+*/
+void op_pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *head;
-	(void)(line_number);
-	head = *stack;
-	while (head != NULL)
-	{
+	stack_t *temp;
 
-		printf("%d\n", head->n);
-		head = head->next;
-		if (head == *stack)
-		{
-			return;
-		}
+	(void)line_number;
+	if (!*stack)
+		return;
+	temp = *stack;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
